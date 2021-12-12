7e66dda7a0bfdfc2f5815f7916cc91cce906fe54
CommanderRedYT <ccommanderred@gmail.com>
2021-12-12 01:20:37 +0100

    Added qr code library

--- src/qrcode.cpp
+++ src/qrcode.cpp
@@ -1,23 +1,24 @@
-#include <Arduino.h>
 #include "qrcode.h"
 #include "qrencode.h"
+
+#include <tftinstance.h>
+
+using namespace espgui;
+
+namespace qrcode {
 
 int offsetsX;
 int offsetsY;
 int screenwidth;
 int screenheight;
-int multiply=2;
 
-QRcode::QRcode(TFT_eSPI *tft){
-  this->tft = tft;
-}
-
-void QRcode::init(){
+void QRcode::init()
+{
     //((TFT_eSPI *)tft)->init();
-    screenwidth = tft->width();
-    screenheight = tft->height(); 
-    //tft->setRotation(1);
-    //tft->fillScreen(TFT_WHITE);
+    screenwidth = tft.width();
+    screenheight = tft.height();
+    //tft.setRotation(1);
+    //tft.fillScreen(TFT_WHITE);
     int min = screenwidth;
     if (screenheight<screenwidth)
       min = screenheight;
@@ -27,31 +28,47 @@ void QRcode::init(){
 
 }
 
-void QRcode::render(int x, int y, int color){
+void QRcode::setMultiply(int m)
+{
+    multiply = m;
+}
+
+void QRcode::render(int x, int y, int color)
+{
   x=(x*multiply)+offsetsX;
   y=(y*multiply)+offsetsY;
-  if(color==1) {
-    tft->drawPixel(x,y,TFT_BLACK);
-    if (multiply>1) {
-      tft->drawPixel(x+1,y,TFT_BLACK);
-      tft->drawPixel(x+1,y+1,TFT_BLACK);
-      tft->drawPixel(x,y+1,TFT_BLACK);
-    }
+
+  /*
+  for (int i=0;i<multiply;i++)
+  {
+      for (int j=0;j<multiply;j++)
+      {
+          if(color==1) {
+            tft.drawPixel(x+i,y+j,TFT_BLACK);
+          }
+          else {
+            tft.drawPixel(x+i,y+j,TFT_WHITE);
+          }
+      }
   }
-  else {
-    tft->drawPixel(x,y,TFT_WHITE);
-    if (multiply>1) {
-      tft->drawPixel(x+1,y,TFT_WHITE);
-      tft->drawPixel(x+1,y+1,TFT_WHITE);
-      tft->drawPixel(x,y+1,TFT_WHITE);
-    }
+  */
+  if (color == 1)
+  {
+      tft.fillRect(x, y, multiply, multiply, TFT_BLACK);
+  }
+  else
+  {
+      tft.fillRect(x, y, multiply, multiply, TFT_WHITE);
   }
 }
 
-void QRcode::create(String message) {
+void QRcode::create(std::string_view message)
+{
   // create QR code
-  tft->fillScreen(TFT_WHITE);
-  message.toCharArray((char *)strinbuf,260);
+  tft.fillScreen(TFT_WHITE);
+  // message.toCharArray((char *)strinbuf,260);
+  strncpy((char *)strinbuf, message.data(), 259);
+  strinbuf[259] = '\0';
   qrencode();
   // print QR Code
   for (byte x = 0; x < WD; x+=2) {
@@ -79,3 +96,48 @@ void QRcode::create(String message) {
     }
   }
 }
+
+void QRcode::createScaleToFit(std::string_view message, uint8_t border)
+{
+    m_tmpmult = multiply;
+
+    // create QR code
+    tft.fillScreen(TFT_WHITE);
+    // message.toCharArray((char *)strinbuf,260);
+    strncpy((char *)strinbuf, message.data(), 259);
+    strinbuf[259] = '\0';
+    qrencode();
+
+    multiply = tft.width() - border * 2;
+    multiply = multiply / WD;
+
+    // print QR Code
+    for (byte x = 0; x < WD; x+=2) {
+      for (byte y = 0; y < WD; y++) {
+        if ( QRBIT(x,y) &&  QRBIT((x+1),y)) {
+          // black square on top of black square
+          render(x, y, 1);
+          render((x+1), y, 1);
+        }
+        if (!QRBIT(x,y) &&  QRBIT((x+1),y)) {
+          // white square on top of black square
+          render(x, y, 0);
+          render((x+1), y, 1);
+        }
+        if ( QRBIT(x,y) && !QRBIT((x+1),y)) {
+          // black square on top of white square
+          render(x, y, 1);
+          render((x+1), y, 0);
+        }
+        if (!QRBIT(x,y) && !QRBIT((x+1),y)) {
+          // white square on top of white square
+          render(x, y, 0);
+          render((x+1), y, 0);
+        }
+      }
+    }
+
+    multiply = m_tmpmult;
+}
+
+} // namespace
--- src/qrcode.h
+++ src/qrcode.h
@@ -1,13 +1,20 @@
 #include <TFT_eSPI.h>
+#include <string>
+
+namespace qrcode {
 
 class QRcode
 {
 	private:
-		TFT_eSPI *tft;
 		void render(int x, int y, int color);
+        int multiply{5};
+        int m_tmpmult;
 
 	public:
-		QRcode(TFT_eSPI *display);
 		void init();
-		void create(String message);	
+        void create(std::string_view message);
+        void setMultiply(int m);
+        void createScaleToFit(std::string_view message, uint8_t border = 10);
 };
+
+} // namespace
